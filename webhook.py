89d4989373e0580d868de379ba4b225b7b134278
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-09 22:36:13 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,1 +1,11 @@
+from flask import Blueprint, request, jsonify
 
+webhook = Blueprint("webhook", __name__)
+
+@webhook.route("/webhook", methods=["POST"])
+def stripe_webhook():
+    event = request.get_json()
+
+    print(event)
+
+    return jsonify({"received": True}), 200

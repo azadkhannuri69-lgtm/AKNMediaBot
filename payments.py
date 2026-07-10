e4924af6d2b34d0578d15b2d2330ddc77f79cc32
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-10 18:52:47 +0200

    Add telegram_id to create_checkout_session function

--- payments.py
+++ payments.py
@@ -1,12 +1,14 @@
 import stripe
+
 from config import STRIPE_SECRET_KEY
 
 stripe.api_key = STRIPE_SECRET_KEY
 
 
-def create_checkout_session(price_id, success_url, cancel_url):
+def create_checkout_session(price_id, success_url, cancel_url, telegram_id):
     session = stripe.checkout.Session.create(
         mode="subscription",
+        payment_method_types=["card"],
         line_items=[
             {
                 "price": price_id,
@@ -15,6 +17,7 @@ def create_checkout_session(price_id, success_url, cancel_url):
         ],
         success_url=success_url,
         cancel_url=cancel_url,
+        client_reference_id=str(telegram_id),
     )
 
     return session.url

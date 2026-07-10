246c05cbe6dfcb973a6ae9a422e7078d96a66b50
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-10 19:05:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 app.register_blueprint(webhook)
 
+
 @app.route("/")
 def home():
-    return "AKN Films Web Service is running!"
+    return "AKN Media Web Service is running!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)

918321f4fb55e47c29549271405b35896ba90a04
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-10 19:54:35 +0200

    Refactor create_checkout_session function parameters

--- payments.py
+++ payments.py
@@ -5,19 +5,37 @@ from config import STRIPE_SECRET_KEY
 stripe.api_key = STRIPE_SECRET_KEY
 
 
-def create_checkout_session(price_id, success_url, cancel_url, telegram_id):
+def create_checkout_session(
+    price_id,
+    success_url,
+    cancel_url,
+    telegram_id,
+):
+
     session = stripe.checkout.Session.create(
         mode="subscription",
-        payment_method_types=["card"],
+
+        payment_method_types=[
+            "card",
+        ],
+
         line_items=[
             {
                 "price": price_id,
                 "quantity": 1,
             }
         ],
+
         success_url=success_url,
+
         cancel_url=cancel_url,
+
         client_reference_id=str(telegram_id),
+
+        metadata={
+            "telegram_id": str(telegram_id),
+            "plan": price_id,
+        },
     )
 
     return session.url

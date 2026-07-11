7240a26f03b6aa3e0beeda35f28a794d30177766
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-11 15:19:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,15 @@ def home():
     return "AKN Media Web Service is running!"
 
 
+@app.route("/success")
+def success():
+    return "Payment Successful"
+
+
+@app.route("/cancel")
+def cancel():
+    return "Payment Cancelled"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)

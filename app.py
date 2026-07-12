f100eb10466f0e7f0d720181d93623261c891aa0
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-12 22:51:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,3 @@ app.register_blueprint(webhook)
 @app.route("/")
 def home():
     return "AKN Media Bot is running."
-
-@app.route("/success")
-def success():
-    return "✅ Payment Successful. You can return to the bot."
-
-@app.route("/cancel")
-def cancel():
-    return "❌ Payment Cancelled."

fc7ce56f5056a69246f0d7dfe1d68f21a29d2dae
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-07-12 22:37:44 +0200

    Refactor checkout session to use price map

--- payments.py
+++ payments.py
@@ -1,31 +1,16 @@
 import stripe
-
-from config import (
-    STRIPE_SECRET_KEY,
-    BASE_URL,
-)
+from config import STRIPE_SECRET_KEY, BASE_URL, PRICE_1_WEEK, PRICE_1_MONTH, PRICE_3_MONTHS, PRICE_12_MONTHS
 
 stripe.api_key = STRIPE_SECRET_KEY
+PRICE_MAP = {"week": PRICE_1_WEEK, "month": PRICE_1_MONTH, "3months": PRICE_3_MONTHS, "12months": PRICE_12_MONTHS}
 
-
-def create_checkout_session(price_id, telegram_id):
+def create_checkout_session(plan, telegram_id):
     session = stripe.checkout.Session.create(
         mode="subscription",
         payment_method_types=["card"],
-        line_items=[
-            {
-                "price": price_id,
-                "quantity": 1,
-            }
-        ],
+        line_items=[{"price": PRICE_MAP[plan], "quantity": 1}],
         client_reference_id=str(telegram_id),
-        metadata={
-            "telegram_id": str(telegram_id),
-            "price_id": price_id,
-        },
-        success_url=f"{BASE_URL}/success?session_id={{CHECKOUT_SESSION_ID}}",
-        cancel_url=f"{BASE_URL}/cancel",
-        allow_promotion_codes=True,
+        success_url=f"{BASE_URL}/success",
+        cancel_url=f"{BASE_URL}/cancel"
     )
-
     return session.url

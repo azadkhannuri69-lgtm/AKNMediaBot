4ede75a4dbdd968dedeae5c04f13bb8245439751
azadkhannuri69-lgtm <azadkhannuri69@gmail.com>
2026-08-02 22:49:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,7 @@
 import os
 
-TOKEN = os.getenv("TOKEN")
-
-BASE_URL = os.getenv("BASE_URL")
-
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
-
-STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
-
-PRICE_1_WEEK = os.getenv("PRICE_1_WEEK")
-
-PRICE_1_MONTH = os.getenv("PRICE_1_MONTH")
-
-PRICE_3_MONTHS = os.getenv("PRICE_3_MONTHS")
-
-PRICE_12_MONTHS = os.getenv("PRICE_12_MONTHS")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+OWNER_ID = os.getenv("OWNER_ID")
